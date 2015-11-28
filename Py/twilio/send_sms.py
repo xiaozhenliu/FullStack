4cf1d044cee9f47343122fa07414500c7ee77b19
xiaozhenliu <zjulxz@gmail.com>
2015-11-28 21:18:33 +0800

    Update send_sms.py

--- Py/twilio/send_sms.py
+++ Py/twilio/send_sms.py
@@ -1,11 +1,12 @@
 from twilio.rest import TwilioRestClient
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "AC32a3c49700934481addd5ce1659f04d2"
-auth_token  = "{{ auth_token }}"
-client = TwilioRestClient(account_sid, auth_token)
+ACCOUNT_SID = "AC1ee9e019a4caea947d0941797b32e141"
+AUTH_TOKEN = "ffe7df1f9f7b97e36f1b165e249b5043"
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
-message = client.messages.create(body="Jenny please?! I love you <3",
-    to="+14159352345",    # Replace with your phone number
-    from_="+14158141829") # Replace with your Twilio number
-print message.sid
+message = client.messages.create(body="I am testing twilio bro <3",
+    to="+19192000708",    # Replace with your phone numbers
+    from_="+19195337660") # Replace with your Twilio number
+
+print(message.sid)
